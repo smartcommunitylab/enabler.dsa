42b4300a22ee1923c7b0c9ce4e2135f45ced209f
gioadami <giordano.adami@gmail.com>
2017-12-12 14:26:15 +0100

    Updates for domain, manager and users.

---
+++ dsa-engine/src/main/java/it/smartcommunitylab/dsaengine/model/Manager.java
@@ -0,0 +1,62 @@
+package it.smartcommunitylab.dsaengine.model;
+
+public class Manager {
+
+	private String id;
+	
+	private String email;	
+	
+	private boolean owner;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isOwner() {
+		return owner;
+	}
+
+	public void setOwner(boolean owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (owner ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manager other = (Manager) obj;
+		if (owner != other.owner)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Manager [id=" + id + ", email=" + email + ", owner=" + owner + "]";
+	}	
+	
+}
--- dsa-engine/src/main/java/it/smartcommunitylab/dsaengine/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package it.smartcommunitylab.dsaengine.model;
-
-public enum UserRole {
-
-	DOMAIN_OWNER, DOMAIN_MANAGER, DOMAIN_USER, DATASET_USER
-	
-}
