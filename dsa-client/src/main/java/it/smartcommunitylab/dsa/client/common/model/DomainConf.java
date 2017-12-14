321917ecdbc048fb1e6c5e611177f7ddf5566c81
gioadami <giordano.adami@gmail.com>
2017-12-14 17:07:34 +0100

    Model updated.

---
+++ dsa-client/src/main/java/it/smartcommunitylab/dsa/client/common/model/DomainConf.java
@@ -0,0 +1,56 @@
+package it.smartcommunitylab.dsa.client.common.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DomainConf extends BaseObject {
+	
+	private ConfigurationProperties defaultConfigurationProperties;
+	
+	private String elasticUser;
+	private String elasticPassword;	
+
+	private List<Manager> managers = new ArrayList<Manager>();
+	private List<User> users = new ArrayList<User>();
+
+	public List<Manager> getManagers() {
+		return managers;
+	}
+
+	public void setManagers(List<Manager> manager) {
+		this.managers = manager;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public ConfigurationProperties getDefaultConfigurationProperties() {
+		return defaultConfigurationProperties;
+	}
+
+	public void setDefaultConfigurationProperties(ConfigurationProperties defaultConfigurationProperties) {
+		this.defaultConfigurationProperties = defaultConfigurationProperties;
+	}
+
+	public String getElasticUser() {
+		return elasticUser;
+	}
+
+	public void setElasticUser(String elasticUser) {
+		this.elasticUser = elasticUser;
+	}
+
+	public String getElasticPassword() {
+		return elasticPassword;
+	}
+
+	public void setElasticPassword(String elasticPassword) {
+		this.elasticPassword = elasticPassword;
+	}
+	
+}
