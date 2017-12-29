da05d71b15604271493d0afe2587575d8aeb8eb9
labu4bd <labu4bd@gmail.com>
2017-12-29 11:23:19 +0100

    Tooltip added in icone
    Tooltip added in icone

--- dsa-console/src/app/custom-material.modul.ts
+++ dsa-console/src/app/custom-material.modul.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'; import { HttpModule } from '@angular/http';
-import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule } from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule, MatTooltipModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule, MatTooltipModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule, MatSelectModule, MatDialogModule, MatTooltipModule],
   declarations: []
 })
 export class CustomMaterialModule { }
