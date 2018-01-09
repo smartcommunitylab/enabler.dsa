1f4d5a8da69fdd1d16246006fb15cbbd95482310
gioadami <giordano.adami@gmail.com>
2018-01-09 11:28:58 +0100

    Added @JsonInclude(Include.NON_NULL).

--- dsa-engine/src/main/java/it/smartcommunitylab/dsaengine/model/ProfileDomainData.java
+++ dsa-engine/src/main/java/it/smartcommunitylab/dsaengine/model/ProfileDomainData.java
@@ -1,5 +1,9 @@
 package it.smartcommunitylab.dsaengine.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class ProfileDomainData {
 
 	private String domain;
